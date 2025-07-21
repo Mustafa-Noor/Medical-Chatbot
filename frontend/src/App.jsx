9bdf90e4b5cc332d010d12aea1074ffa7c5ca4a3
marwahkhokher <marwah.khokher18@gmail.com>
2025-07-21 16:25:34 +0500

    frontend ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterForm from "./components/RegisterForm";
 import LoginForm from "./components/LoginForm";
+import HomePage from "./components/HomePage"; // <-- import
 
 function App() {
   return (
-    <>
-      <RegisterForm />
-      <hr />
-      <LoginForm />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/login" element={<LoginForm />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
