f364fa1241e6d64022f654fb3352cb6547f57fed
Mustafa-Noor <mus.noor12@gmail.com>
2025-07-24 14:34:58 +0500

    trying

--- backend/app/routes/topics.py
+++ backend/app/routes/topics.py
@@ -15,6 +15,9 @@ def get_topics(current_user: dict = Depends(deps.get_current_user)):
         return {"error": str(e)}
 
 
+#this is making me changes just 
+
+
 # # ✅ POST: save selected topic for a user
 # @router.post("/user/topic")
 # async def save_user_topic(selection: TopicSelection, db: AsyncSession = Depends(get_db)):
